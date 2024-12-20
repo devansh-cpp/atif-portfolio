c36455653a328abafbd3425f553e79ffb87b6496
devansh-cpp <113198313+devansh-cpp@users.noreply.github.com>
2024-12-21 01:44:55 +0530

    port-v-1.02

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <title>Atif Portfolio</title>
+      </head>
       <body
       
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
